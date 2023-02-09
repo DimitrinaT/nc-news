15910df3d1daef495bb1b077e1ac7ae98b52acce
DimitrinaT <dimitrina22tancheva@gmail.com>
2023-02-09 09:00:52 +0000

    addinng tasks 5/6

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { ArticleList } from "./components/ArticlesList";
 import Header from "./components/Header";
+import Article from "./components/Article";
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-      <ArticleList />
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Header />
+        <Routes>
+          <Route path="/" element={<ArticleList />} />
+          <Route path="/articles/:article_id" element={<Article />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
--- src/components/ArticlesList.jsx
+++ src/components/ArticlesList.jsx
@@ -2,32 +2,32 @@ import {useState, useEffect} from 'react'
 import { ArticleCard } from './ArticalCard'
 
 
+
+
 export const ArticleList = () => {
     const [articles, setArticles] = useState([]);
   
     useEffect(() => {
       fetch('https://dimitrina-news.onrender.com/api/articles')
         .then((res) => res.json())
-        .then((data) => 
-          
-        setArticles(data.articles)
-        
+        .then((data) => setArticles(data.articles)
         );
         
-    }, []);
+}, []);
     
     
 
 return (
-    <div className="article-list">
-      {articles.length > 0 && articles.map((article) => (
-        <ArticleCard
-          key={article.article_id} 
-             title={article.title}
-                 article_img_url={article.article_img_url}
-        />
-
-      ))}
-    </div>
+  <div className="article-list">
+  {articles.map((article) => (
+  <ArticleCard
+           key={article.article_id}
+           title={article.title}
+           article_img_url={article.article_img_url}
+           article_id={article.article_id}
+         />
+  
+  ))}
+  </div>
   );
-      };
+  };
